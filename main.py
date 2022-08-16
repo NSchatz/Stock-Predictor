2e21323a870a1d89ab2c020da220b40751d0aa3e
NSchatz <noah.lane.schatz@gmail.com>
2022-08-16 13:43:51 -0400

    took json from api and convert it into usable data. added plot figure

--- main.py
+++ main.py
@@ -16,3 +16,29 @@ ticker = 'TSLA' #Tesla
 
 url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=%s&outputsize=full&apikey=%s"%(ticker,api_key)
 file_save = 'stock_market_data-%s.csv'%ticker
+
+if not os.path.exists(file_save):
+    with urllib.request.urlopen(url) as url:
+        data = json.loads(url.read().decode())
+        data = data['Time Series (Daily)']
+        df = pd.DataFrame(columns=['Date','Low','High','Close','Open'])
+        for k,v in data.items():
+                date = dt.datetime.strptime(k, '%Y-%m-%d')
+                data_row = [date.date(),float(v['3. low']),float(v['2. high']),
+                            float(v['4. close']),float(v['1. open'])]
+                df.loc[-1,:] = data_row
+                df.index = df.index + 1
+        print('Data saved to : %s'%file_save)        
+else:
+    print('File already exists. Loading data from CSV')
+    df = pd.read_csv(file_save)
+
+df = df.sort_values('Date')
+df.head()
+
+plt.figure(figsize = (18,9))
+plt.plot(range(df.shape[0]),(df['Low']+df['High'])/2.0)
+plt.xticks(range(0,df.shape[0],500),df['Date'].loc[::500],rotation=45)
+plt.xlabel('Date',fontsize=18)
+plt.ylabel('Mid Price',fontsize=18)
+plt.show()
